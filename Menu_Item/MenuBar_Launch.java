12da30ba4117990089fefef15bcde4f3eb2783db
Kaliraj T <kalirajk217@gmail.com>
2024-08-09 20:20:19 +0530

    Update MenuBar_Launch.java

--- Menu_Item/MenuBar_Launch.java
+++ Menu_Item/MenuBar_Launch.java
@@ -1,1 +1,10 @@
+package AWT_Swing_Projects.Menu_Items;
+import java.lang.*;
 
+public class MenuBar_Launch
+{
+    public static void main(String[] args)
+    {
+        new MenuBar_Process();
+    }
+}
